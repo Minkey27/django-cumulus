64b582208a478c6fcc6c17e7497d34c433358518
Yi Ming Yung <yiming.yung@outlook.com>
2019-12-10 14:25:55 +0100

    Sigh

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     cmdclass = versioneer.get_cmdclass(),
     packages = find_packages(),
     install_requires = [
-        "http://github.com/Minkey27/pyrax/tarball/9d4722e83d4c7c66fd63f040321ead62f67efba6#egg=pyrax",
+        "pyrax @ git+https://github.com/Minkey27/pyrax@9d4722e83d4c7c66fd63f040321ead62f67efba6",
     ],
     author = "Ferrix Hovi, Thomas Schreiber",
     license = "BSD",
